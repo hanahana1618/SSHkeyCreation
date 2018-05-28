d979e0a4e318813ea9e943e4fe7f7164a1cef06f
Motoko <hanahana1.618@hotmail.es>
2018-05-28 16:26:47 -0400

    pseudo code

---
+++ ssh-pseudo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#bash script to create a new ssh-keygen key
+
+#ask user for 
+#github email address
+#install clip: sudo apt-get install geomview
+#echo 
+
+#check for existing SSH keys
+ls -al ~/.ssh
+#echo $existingkeys
+#ask the user for input here to continue or not
+
+#generate new ssh-keys
+#ssh-keygen -t rsa -b 4096 -C "email"
